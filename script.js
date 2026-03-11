4e06af55c33477c0f427a451700622de0615e0c6
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-03-11 08:58:27 -0300

    Inicial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Jeba Grande");
+
+let nome = "Daniel Felipe Sehnem";
+console.log(nome)
+
+const pi = 3.14159265358979
+console.log(pi)
+
+let n1 = 1;
+let n2 = 2;
+
+console.log(n1 + n2);
+
+function ola(){
+    console.log("Olá, " + nome);
+}
+
+function somar(n3,n4){
+    console.log(n3 + n4);
+}
+
+somar(1, 22);
+ola();
